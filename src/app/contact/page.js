4541eb7a9a31c5aac3477c79163315900c15d2a1
hawinf <hawinf54@gmail.com>
2025-11-04 04:43:40 +0400

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <main>
+            <h1>Contact SCHNEIDER</h1>
+        </main>
+    );
+};
+
+export default page;
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,106 @@
+"use client";
+import React, {useState, useRef, useEffect} from "react";
+import Link from "next/link";
+import "./menu.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const menuLinks = [
+    { path: "/", label: "Home"},
+    { path: "about", label: "About"},
+    { path: "contact", label: "Contact"},
+    { path: "properties", label: "Properties"},
+];
+
+const Menu = () => {
+    const container = useRef();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const tl = useRef();
+
+    useGSAP(() => {
+        gsap.set(".menu-link-item-holder", {y: 75});
+        tl.current = gsap.timeline({ paused: true })
+        .to(".menu-overlay", {
+            duration: 1.25,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "power4.inOut",
+        })
+        .to(".menu-link-item-holder", {
+            y: 0,
+            duration: 1,
+            stagger: 0.1,
+            ease: "power4.inOut",
+            delay: -0.75,
+        })
+    }, 
+    {scope: container}
+);
+
+useEffect(() => {
+    if (isMenuOpen) {
+        tl.current.play()
+    } else {
+        tl.current.reverse();
+    }
+}, [isMenuOpen]);``
+
+
+
+
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    return (
+        <main className="menu-container" ref={container}>
+            <nav className="menu-bar">
+                <main className="menu-logo">
+                    <Link href="/">SCHNEIDER LOGO</Link>
+                </main>
+                <main className="menu-open" onClick={toggleMenu}>
+                    <p>Menu</p>
+                </main>
+            </nav>
+            <div className="menu-overlay">
+                <div className="menu-overlay-bar">
+                    <div className="menu-logo">
+                        <Link href="/">SCHNEIDER</Link>
+                    </div>
+                    <div className="menu-close" onClick={toggleMenu}>
+                        <p>Close</p>
+                    </div>
+                </div>
+                <div className="menu-close-icon">
+                    <p>&#x2715;</p>
+                </div>
+                <div className="menu-copy">
+                    <div className="menu-links">
+                        {menuLinks.map((link, index) => (
+                            <div className="menu-link-item" key={index}>
+                                <div className="menu-link-item-holder" onClick={toggleMenu}>
+                                    <Link href={link.path} className="menu-link">
+                                    {link.label}
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="menu-info">
+                        <div className="menu-info-col">
+                            <a>Instagram</a>
+                        </div>
+                        <div className="menu-info-col">
+                            <a>Dubai AE</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="menu-preview">
+                    <p>VIEW SCHNEIDER</p>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Menu;
