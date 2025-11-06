c7f98f74d5b9e711183723c1cbae45079543e2f5
hawinf <hawinf54@gmail.com>
2025-11-06 05:21:02 +0400

    x

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,6 +3,7 @@ import './about.css';
 
 const About = () => {
     return (
+        <>
         <main className="about-secton" >
             <nav className="about-left">
                 <h1 data-aos="fade-up">SCHNEIDER&reg;</h1>
@@ -10,6 +11,12 @@ const About = () => {
                 <p data-aos="slide-left">At Schneider Real Estate, we believe finding the perfect home is more than just a transaction—it’s a journey. Our experienced team is here to guide you every step of the way, helping you discover a place where you feel truly comfortable and at home. Whether you’re buying, renting, or investing, we provide personalized support, expert market insights, and practical advice to make your real estate experience smooth and stress-free. With years of experience in the UAE property market, we are committed to helping you make informed decisions and ensuring that your move is seamless—from the first viewing to settling into your new home. At Schneider Real Estate, your comfort and satisfaction are our top priority.</p>
             </nav>
         </main>
+        <main className="why-us">
+            <h1>WHY US</h1>
+            <p>At Schneider Real Estate, we go beyond simply helping you find a property. We guide you every step of the way, ensuring you feel confident and comfortable in your decisions. Our team of experienced professionals knows the UAE real estate market inside out, providing personalized advice, timely support, and transparent communication. Whether you’re buying, renting, or investing, we focus on making the process seamless and stress-free. With Schneider Real Estate, you don’t just find a place—you find a home where you can thrive.</p>
+            
+        </main>
+        </>
     );
 };
 
