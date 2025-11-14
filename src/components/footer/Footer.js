4ed6feefc95dada0ee88aa46cfe5c24aef3fbeaa
hawinf <hawinf54@gmail.com>
2025-11-14 15:21:04 +0400

    adding footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import './footer.css';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="footer-section">
             <h1>SCHNEIDER</h1>
+            <p>Real Estate</p>
+            <h3>We don’t just find properties — we help you find the place where you truly belong. Your comfort, our commitment.</h3>
+            <nav className="footer-menu">
+                <div className="each-menu">
+                    <Link href=''>HOME</Link>
+                </div>
+                <div className="each-menu">
+                    <Link href=''>BUY</Link>
+                </div>
+                <div className="each-menu">
+                    <Link href=''>RENT</Link>
+                </div>
+                <div className="each-menu">
+                    <Link href=''>OFF PLAN</Link>
+                </div>
+                <div className="each-menu">
+                    <Link href=''>CONTACT</Link>
+                </div>
+            </nav>
+            <main className="policy-section">
+                <div className="each-item">
+                    <Link href=''>&middot; Privacy policy</Link>
+                </div>
+                <div className="each-item">
+                    <Link href=''>&middot; Terms and Conditions</Link>
+                </div>
+                <div className="each-item">
+                    <Link href=''>&middot; Cookie Preferences</Link>
+                </div>
+            </main>
         </footer>
     );
 };
